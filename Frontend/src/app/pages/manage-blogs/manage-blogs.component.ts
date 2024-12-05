95aeaf3e7e8a8f82bf7f0eb784f62dd5253f0632
Praneeth Kumar <124180295+PraneethKumar189@users.noreply.github.com>
2024-12-05 22:39:10 +0530

    created manageblog -component

--- Frontend/src/app/pages/manage-blogs/manage-blogs.component.ts
+++ Frontend/src/app/pages/manage-blogs/manage-blogs.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiserviceService } from '../../services/apiservice.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-manage-blogs',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './manage-blogs.component.html',
   styleUrl: './manage-blogs.component.css'
 })
 export class ManageBlogsComponent implements OnInit {
   id:any;
+  res:any;
+  cent:string="center";
   constructor(private route:ActivatedRoute,private api:ApiserviceService){}
  ngOnInit(): void {
    this.id=this.route.snapshot.paramMap.get('profid1')!;
@@ -18,6 +21,7 @@ export class ManageBlogsComponent implements OnInit {
 
    this.api.getblogsbyid(this.id).subscribe(data=>{
     console.log(data)
+    this.res=data;
    })
  }
 }
