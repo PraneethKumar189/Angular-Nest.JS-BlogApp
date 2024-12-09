201a364f04a21fb3b8e248cfef485a9e9d5ab344
Praneeth Kumar <124180295+PraneethKumar189@users.noreply.github.com>
2024-12-09 15:04:15 +0530

    added deleteusercomponent

--- Frontend/src/app/admin/delete-user/delete-user.component.ts
+++ Frontend/src/app/admin/delete-user/delete-user.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdminComponent } from '../admin/admin.component';
 
 @Component({
   selector: 'app-delete-user',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AdminComponent],
   templateUrl: './delete-user.component.html',
   styleUrl: './delete-user.component.css'
 })
